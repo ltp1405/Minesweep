959c7abb67e1cec966a2cbad80bad40fced0b6a6
ltp1405 <LamTungPro1405@gmail.com>
2021-12-28 00:46:21 +0700

    Combine PushButton and GameMenu

--- Button.cpp
+++ Button.cpp
@@ -4,8 +4,7 @@
 using namespace std;
 
 bool InBound(sf::Vector2f pos, sf::Vector2f from, sf::Vector2f to) {
-    if (pos.x > from.x && pos.y > from.y &&
-            pos.x < to.x && pos.y < to.y)
+    if (pos.x > from.x && pos.y > from.y && pos.x < to.x && pos.y < to.y)
         return true;
     return false;
 }
@@ -35,65 +34,52 @@ void PushButton::draw(sf::RenderWindow &window) {
     txt.setCharacterSize(mFontSize);
     txt.setFillColor(mTextColor);
     sf::Vector2f pos = mPosition;
-    sf::Vector2f textPos = pos + sf::Vector2f(mSize/2.f);
+    sf::Vector2f textPos = pos + sf::Vector2f(mSize / 2.f);
 
-    txt.setOrigin(text.length()/2 * mFontSize * 0.8, mFontSize*0.5);
+    txt.setOrigin(text.length() / 2 * mFontSize * 0.8, mFontSize * 0.5);
     txt.setPosition(textPos);
 
     window.draw(rect);
     window.draw(txt);
 }
 
-void PushButton::move(float x, float y) {
-    mPosition = sf::Vector2f(x, y);
-}
+void PushButton::move(float x, float y) { mPosition = sf::Vector2f(x, y); }
 
-void PushButton::setSize(float x, float y) {
-    mSize = sf::Vector2f(x, y);
-}
+void PushButton::setSize(float x, float y) { mSize = sf::Vector2f(x, y); }
 
 void PushButton::handleEvent(sf::Event e) {
     sf::Vector2f to = mSize + mPosition;
     sf::Vector2f from = mPosition;
 
     switch (e.type) {
-        case sf::Event::MouseMoved:
-        {
-            sf::Vector2f mousePos = sf::Vector2f(e.mouseMove.x, e.mouseMove.y);
-            if (InBound(mousePos, from, to))
-                hovered = true;
-            else hovered = false;
-            break;
-        }
+    case sf::Event::MouseMoved: {
+        sf::Vector2f mousePos = sf::Vector2f(e.mouseMove.x, e.mouseMove.y);
+        if (InBound(mousePos, from, to))
+            hovered = true;
+        else
+            hovered = false;
+        break;
+    }
 
-        case sf::Event::MouseButtonPressed:
-        {
-            sf::Vector2f mousePos = sf::Vector2f(e.mouseButton.x, e.mouseButton.y);
-            if (InBound(mousePos, from, to))
-                clicked = true;
-            else clicked = false;
-            break;
-        }
-
-        case sf::Event::MouseButtonReleased:
-        {
+    case sf::Event::MouseButtonPressed: {
+        sf::Vector2f mousePos = sf::Vector2f(e.mouseButton.x, e.mouseButton.y);
+        if (InBound(mousePos, from, to))
+            clicked = true;
+        else
             clicked = false;
-        }
+        break;
+    }
+
+    case sf::Event::MouseButtonReleased: {
+        clicked = false;
+    }
     }
 }
 
-void PushButton::setText(string txt) {
-    text = txt;
-}
+void PushButton::setText(string txt) { text = txt; }
 
-void PushButton::setFillColor(sf::Color color) {
-    mFillColor = color;
-}
+void PushButton::setFillColor(sf::Color color) { mFillColor = color; }
 
-void PushButton::setHoverColor(sf::Color color) {
-    mHoverColor = color;
-}
+void PushButton::setHoverColor(sf::Color color) { mHoverColor = color; }
 
-void PushButton::setTextColor(sf::Color color) {
-    mTextColor = color;
-}
+void PushButton::setTextColor(sf::Color color) { mTextColor = color; }
